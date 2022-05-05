36a48eab79fb8621f822842295fc8a0bb2fcc024
IKSHAKU GOSWAMI <ikshakugoswami@IKSHAKUs-MacBook-Air.local>
2022-05-05 11:46:59 -0700

    added 2 quizzes, created base prototype

--- dist/script.js
+++ dist/script.js
@@ -1,91 +1,61 @@
-/*$(function() {
- $('.slide').css({
-   'height': (($(window).height()) -40)
-  });
-});*/
 
-/*$(function(){
-  setHeight();
-});
-$(window).resize(function() {
-  setHeight();
-});
-function setHeight() {
-  var Height = $(window).height();
-  $('.slide').css('height', Height);
-}
-*/
-const questions = [
-  {
-      question: "In the school playground, who picked Hamad in their team?",
-      choices: ["a) Khalid", "b) Faisal", "c) Hamad did not participate", "d) Nobody picked Hamad in their team"],
-      answer: "d) Nobody picked Hamad in their team"
-  },
-]
+
+// import fs from 'fs'
+// const fs = require('fs')
+// const content = 'Some content!';
+
+// fs.writeFile('test.txt', content, err => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   // file written successfully
+// });
 
 var timer = document.getElementById("timer");
 var startDiv = document.getElementById("start");
 var startQuizBtn = document.getElementById("start-quiz-button");
+var startQuizBtn2 = document.getElementById("start-quiz-button2");
+
 var questionDiv = document.getElementById("questionDiv");
 var questionTitle = document.getElementById("questionTitle");
+var questionDiv2 = document.getElementById("questionDiv2");
+var questionTitle2 = document.getElementById("questionTitle2");
 var choiceA = document.getElementById("btn0");
 var choiceB = document.getElementById("btn1");
 var choiceC = document.getElementById("btn2");
 var choiceD = document.getElementById("btn3");
-//var answerCheck = document.getElementById("answerCheck");
+var choiceA2 = document.getElementById("btn4");
+var choiceB2 = document.getElementById("btn5");
+var choiceC2 = document.getElementById("btn6");
+var choiceD2 = document.getElementById("btn7");
 var initialInput = {};
 
 var correctAns = 0;
 var questionNum = 0;
-var timeLeft = {};
-/**
-* FUNCTIONS
-*/
 
-// WHEN I click the start button, timer starts
-
-var totalTime = 151;
-
-
-
-function goToByScroll(id){			$('html,body').animate({
+function goToByScroll(id){$('html,body').animate({
 scrollTop: $("."+id).offset().top +0
 },400);
 console.log("Time taken to read: " + (new Date()).getSeconds() + " seconds");
 }
 
-var totalTime = 151;
 function newQuiz() {
-  questionIndex = 0;
-  totalTime = 150;
-//    timeLeft.textContent = totalTime;
-  initialInput.textContent = "";
-
-  startDiv.style.display = "none";
-  questionDiv.style.display = "block";
-  // timer.style.display = "block";
-  // timesUp.style.display = "none";
-
-  var startTimer = setInterval(function() {
-      totalTime--;
-      timeLeft.textContent = totalTime;
-      if(totalTime <= 0) {
-          clearInterval(startTimer);
-          if (questionIndex < questions.length - 1) {
-              gameOver();
-          }
-      }
-  },1000);
-
-  showQuiz();
+  nextQuestion();
 };
 
-// then presented with questions and choices
-function showQuiz() {
-  nextQuestion();
-}
+function newQuiz2() {
+  nextQuestion2();
+};
 
 function nextQuestion() {
+  questionIndex = 0;
+  const questions = [
+    {
+        question: "In the school playground, who picked Hamad in their team?",
+        choices: ["a) Khalid", "b) Faisal", "c) Hamad did not participate", "d) Nobody picked Hamad in their team"],
+        answer: "d) Nobody picked Hamad in their team"
+    }
+  ]
   questionTitle.textContent = questions[questionIndex].question;
   choiceA.textContent = questions[questionIndex].choices[0];
   choiceB.textContent = questions[questionIndex].choices[1];
@@ -93,18 +63,34 @@ function nextQuestion() {
   choiceD.textContent = questions[questionIndex].choices[3];
 }
 
+function nextQuestion2() {
+  questionIndex = 0;
+  const questions = [
+    {
+        question: "Do you think students will be friends with Hamad?",
+        choices: ["a) Yes, because new student", "b) No", "c) No", "d) yes"],
+        answer: "b) No"
+    }
+  ]
+  questionTitle2.textContent = questions[questionIndex].question;
+  choiceA2.textContent = questions[questionIndex].choices[0];
+  choiceB2.textContent = questions[questionIndex].choices[1];
+  choiceC2.textContent = questions[questionIndex].choices[2];
+  choiceD2.textContent = questions[questionIndex].choices[3];
+}
+
 
 // after question is answered, show if correct or wrong
-function checkAnswer(answer) {
-
-  //var lineBreak = document.getElementById("lineBreak");
-  //lineBreak.style.display = "block";
-  //answerCheck.style.display = "block";
-
-  if (questions[questionIndex].answer === questions[questionIndex].choices[answer]) {
-      // correct answer, add 1 score to final score
-      correctAns++;
-      // console.log(correctAns);
+function checkAnswer(ans) {
+questionIndex = 0;
+  var questions = [
+    {
+        question: "In the school playground, who picked Hamad in their team?",
+        choices: ["a) Khalid", "b) Faisal", "c) Hamad did not participate", "d) Nobody picked Hamad in their team"],
+        answer: "d) Nobody picked Hamad in their team"
+    }
+  ]
+  if (questions[questionIndex].answer === questions[questionIndex].choices[ans]) {
       var emoji = document.createElement("img");
       emoji.src = 'smiley-emoji.gif';
       emoji.alt = "Yayyy!";
@@ -112,13 +98,10 @@ function checkAnswer(answer) {
       emoji.width = "200";
       document.getElementById("answerCheck").appendChild(emoji);
       console.log("You took" + " " + (new Date()).getSeconds() + " seconds for the quiz") ;
-      //answerCheck.textContent = "Correct! Emoji will come";
-      // answerCheck.src = smiley-emoji.gif;
-      // answerCheck.alt = "Smiley emoji";
+      var wrong = document.getElementById("wrongAns");
+      wrong.style.display = "block";
+      wrong.textContent = "Super!";
   } else {
-      // wrong answer, deduct 10 second from timer
-      //totalTime -= 10;
-      //timeLeft.textContent = totalTime;
       var emoji = document.createElement("img");
       emoji.src = 'wrong.gif';
       emoji.alt = "Try Again";
@@ -130,15 +113,42 @@ function checkAnswer(answer) {
       wrong.textContent = "Oops! Better Luck Next Time!";
       console.log("You took" + " " + (new Date()).getSeconds() + "seconds") ;
   }
+}
+
+function checkAnswer2(ans) {
+  questionIndex = 0;
+  var questions = [
+    {
+        question: "Do you think students will be friends with Hamad?",
+        choices: ["a) Yes, because new student", "b) No", "c) No", "d) yes"],
+        answer: "b) No"
+    }
+  ]
+  if (questions[questionIndex].answer === questions[questionIndex].choices[ans]) {
+      var emoji = document.createElement("img");
+      emoji.src = 'smiley-emoji.gif';
+      emoji.alt = "Yayyy!";
+      emoji.height = "200";
+      emoji.width = "200";
+      document.getElementById("answerCheck2").appendChild(emoji);
+      console.log("You took" + " " + (new Date()).getSeconds() + " seconds for the quiz") ;
+      var wrong = document.getElementById("wrongAns2");
+      wrong.style.display = "block";
+      wrong.textContent = "Super!";
+  } else {
+      var emoji = document.createElement("img");
+      emoji.src = 'wrong.gif';
+      emoji.alt = "Try Again";
+      emoji.height = "200";
+      emoji.width = "200";
+      document.getElementById("answerCheck2").appendChild(emoji);
+      var wrong = document.getElementById("wrongAns2");
+      wrong.style.display = "block";
+      wrong.textContent = "Oops! Better Luck Next Time!";
+      console.log("You took" + " " + (new Date()).getSeconds() + "seconds") ;
+  }
 
   questionIndex++;
-  // repeat with the rest of questions
-  // if (questionIndex < questions.length) {
-  //     nextQuestion();
-  // } else {
-  //     // if no more question, run game over function
-  //     gameOver();
-  // }
 }
 
 
@@ -150,11 +160,24 @@ function chooseC() { checkAnswer(2); }
 
 function chooseD() { checkAnswer(3); }
 
+function chooseA2() { checkAnswer2(0); }
+
+function chooseB2() { checkAnswer2(1); }
+
+function chooseC2() { checkAnswer2(2); }
+
+function chooseD2() { checkAnswer2(3); }
+
 /**
 * ADD EVENT LISTENERS
 */
-startQuizBtn.addEventListener("click", newQuiz);
+startQuizBtn.addEventListener("click", nextQuestion);
+startQuizBtn2.addEventListener("click", nextQuestion2);
 choiceA.addEventListener("click", chooseA);
 choiceB.addEventListener("click", chooseB);
 choiceC.addEventListener("click", chooseC);
 choiceD.addEventListener("click", chooseD);
+choiceA2.addEventListener("click", chooseA2);
+choiceB2.addEventListener("click", chooseB2);
+choiceC2.addEventListener("click", chooseC2);
+choiceD2.addEventListener("click", chooseD2);
